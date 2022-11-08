e5bcafc423e31a64440e96c48033db964a16bb5f
PULI GOKULA KISHORE REDDY <94065306+PULI-GOKULA-KISHORE-REDDY@users.noreply.github.com>
2022-11-08 15:31:49 +0530

    Update and rename division_bit to division_bit.java

---
+++ division_bit.java
@@ -0,0 +1,20 @@
+ class Solution {
+  public static int divide(int A, int B) {      //   A/B
+      /* write your solution here */
+      boolean sign=(A>=0 == B>=0)? true:false;
+      A=Math.abs(A);
+      B=Math.abs(B);
+      int result=0;
+      while(A-B >=0)
+      {
+          int count=0;
+          while(A-(B<<1<<count) >=0)
+          {
+              count++;
+          }
+          result+=1<<count;
+          A-=B<<count;
+      }
+      return sign? result:-result;
+  }
+}
